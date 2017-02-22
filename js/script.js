36966d04af1d384ec82da69029349630d48d8420
Irina <italia2505@gmail.com>
2017-02-22 14:57:09 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,32 @@
+﻿$(function(){
+
+	
+
+	$('.fancy').fancybox(
+		{
+    	openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
+
+    	
+ 	})
+
+});
+
+$(function(){
+   if($(window).width() > '800') {
+  $('.bxslider').bxSlider(
+      {
+       pager:false
+      }
+    )
+}
+});
+
+
+
